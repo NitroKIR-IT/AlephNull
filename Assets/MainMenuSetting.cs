3e673a98187bd9880ddd570ce408a1388617b828
Damillora <duniapengetahuan.12345@gmail.com>
2017-04-06 15:43:29 +0700

    add mainmenu package

---
+++ Assets/MainMenuSetting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MainMenuSetting : MonoBehaviour 
+{
+	public Canvas MainCanvas;
+	public Canvas OptionCanvas;
+
+	void Awake()
+	{
+		OptionCanvas.enabled = false;
+	}
+
+	public void OptionsOn()
+	{
+		OptionCanvas.enabled = true;
+		MainCanvas.enabled = false;
+	}
+	public void ReturnOn ()
+	{
+		OptionCanvas.enabled = false;
+		MainCanvas.enabled = true;
+	}
+	public void ChangeScene (string sceneName)
+	{
+		Application.LoadLevel (sceneName);
+	}
+
+}
---
+++ Assets/Soundclick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Soundclick : MonoBehaviour {
+	public AudioSource source;
+	public AudioClip Click;
+	public bool Audion = true;
+
+	public void Turnoff ()
+	{
+		Audion = false;
+	}
+	public void Turnon()
+	{
+		Audion = true;
+	}
+
+	public void Onclick ()
+	{
+		if (Audion == true)
+		{
+			source.PlayOneShot (Click);
+		}
+	}
+}
